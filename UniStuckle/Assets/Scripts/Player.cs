acf9f81c6a584a534384dca7bc7c99767bc5a7fd
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2022-03-01 22:33:55 -0500

    Lean affects how hard you pedal

--- UniStuckle/Assets/Scripts/Player.cs
+++ UniStuckle/Assets/Scripts/Player.cs
@@ -5,25 +5,31 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    Player player;
+
     public float speed;
 
     Rigidbody2D rb;
+
+    public Vector2 playerPosition;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        player = this;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        playerPosition = rb.transform.position;
+
         // get arrow key input (left right)
         float input = Input.GetAxisRaw("Horizontal");
         
         // move the player
         rb.AddForce(transform.right * input * speed);
-    
     }
 }
--- UniStuckle/Assets/Scripts/Unicycle.cs
+++ UniStuckle/Assets/Scripts/Unicycle.cs
@@ -4,27 +4,65 @@ using UnityEngine;
 
 public class Unicycle : MonoBehaviour
 {
-
+    Unicycle unicycle;
+    Player playerScript;
     public float speed;
 
     Rigidbody2D rb;
+
+    public Vector2 playerPosition;
+    public Vector2 unicyclePosition;
+
+    public float lean;
+    public float leanMultiplier;
+    public float antiLeanMultiplier;
+
+    Vector2 pedalPower;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        unicycle = this;
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // get arrow key input (left right)
+        unicyclePosition = rb.transform.position;
+
+        // get arrow key input (up down)
         float input = Input.GetAxisRaw("Vertical");
         
         // move the wheel
-        // rb.velocity = new Vector2(input * speed, rb.velocity.y);
-        // rb.AddForce(transform.right * input * speed);
-        rb.AddForce(new Vector2(1, 0) * input * speed);
+        lean = calculateLean();
+        print(lean);
+        if(lean >= 0 && input > 0) {
+            pedalPower = new Vector2(1, 0) * input * speed * lean * leanMultiplier;
+            rb.AddForce(pedalPower);
+        } 
+        
+        else if (lean >= 0 && input < 0) {
+            pedalPower = new Vector2(1, 0) * input * speed * lean * antiLeanMultiplier;
+            rb.AddForce(pedalPower);
+        } 
+        
+        else if (lean < 0 && input > 0) {
+            pedalPower = new Vector2(1, 0) * input * speed * -lean * antiLeanMultiplier;
+            rb.AddForce(pedalPower);
+        } 
+        
+        else if (lean < 0 && input < 0) {
+            pedalPower = new Vector2(1, 0) * input * speed * -lean * leanMultiplier;
+            rb.AddForce(pedalPower);
+        }
+    }
+
+    private float calculateLean() {
+        playerPosition = playerScript.playerPosition;
+        return playerPosition.x - unicyclePosition.x;
     }
 }
