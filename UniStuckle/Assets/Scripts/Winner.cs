171a8d72f0b41144ca1503bc888f72aa786ed6b1
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2022-09-09 15:32:58 -0400

    gitignore in unistuckle folder and not in github folder

---
+++ UniStuckle/Assets/Scripts/Winner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winner : MonoBehaviour
+{
+    
+    Player playerScript;
+
+    void Start()
+    {
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void OnTriggerEnter2D(Collider2D hitObject) {
+        if(hitObject.tag == "Player") {
+            GameController.instance.ShowGameOverScreen();
+        }
+    }
+}
